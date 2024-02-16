79261b487fc9d457c8e66d61f4d3ee80926fff7a
xxx <dd@dd.dd>
2024-02-16 16:40:40 +0300

    2

--- data/admin.py
+++ data/admin.py
@@ -10,17 +10,18 @@ class LinkInline(admin.TabularInline):
     extra = 0
 
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ('uid','wallet','twitter','get_textt','is_wl','is_viewed','only_twitter',)
+    list_display = ('uid','wallet','twitter','is_wl','is_viewed','only_twitter',)
     search_fields = ('uid','wallet','twitter',)
     list_filter = (
         'is_wl','is_viewed','is_forever','only_twitter',
     )
     model = Note
 
-    def get_textt(self, obj):
-        return obj.text[:20]
-
-    get_textt.short_description = "text"
+    # def get_textt(self, obj):
+    #
+    #     return obj.text[:20]
+    #
+    # get_textt.short_description = "text"
     inlines = [ImageInline, LinkInline]
 
 
