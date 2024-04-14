60982030609906ba9ccbd15cb12bd8233578e0c6
xxx <dd@dd.dd>
2024-04-14 23:28:48 +0300

    2

--- user/views.py
+++ user/views.py
@@ -111,12 +111,12 @@ class BuyUpgrade(APIView):
 
 class Test(APIView):
     def get(self, request):
-        codes = Code.objects.all()
-        for code in codes:
-            code.code = code.code.upper()
-            code.save()
-        # from .tasks import resetClaim
-        # resetClaim()
+        # codes = Code.objects.all()
+        # for code in codes:
+        #     code.code = code.code.upper()
+        #     code.save()
+        from .tasks import resetClaim
+        resetClaim()
         return Response()
 class TxHistory(generics.ListAPIView):
     serializer_class = TransactionSerializer
